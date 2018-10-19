77b6de07b46f1e6a19cd5f5df0a14bb614fc4966
Ondřej Sekáč <onik66@gmail.com>
2018-10-19 19:27:40 +0200

    Add project files.

---
+++ LennyBOTv2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LennyBOTv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
