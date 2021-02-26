5b82a09e002b0c1798aa1505f2e61f2b023ea28a
Ondřej Sekáč <onik66@gmail.com>
2021-02-26 13:13:31 +0100

    add fibonacci spacing

--- LennyBOTv2/Modules/GeneralModule.cs
+++ LennyBOTv2/Modules/GeneralModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
@@ -129,6 +130,41 @@ namespace LennyBOTv2.Modules
             }
             await ReplyAsync(stringBuilder.ToString()).ConfigureAwait(false);
             await Context.Message.DeleteAsync().ConfigureAwait(false);
+        }
+
+        [Command("fibonacci"), Alias("fib")]
+        public async Task FibonacciSpacingCmdAsync([Remainder] string text)
+        {
+            text = text.Replace(" ", "").Replace("\n", "");
+
+            if (text.Length > 16)
+            {
+                await ReplyAsync("too long for discord 😦").ConfigureAwait(false);
+                return;
+            }
+
+            var fibs = new[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
+                                144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711,
+                                28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040,
+                                1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817,
+                                39088169, 63245986, 102334155, 165580141, 267914296, 433494437,
+                                701408733, 1134903170, 1836311903};
+
+            int Fib(int n)
+            {
+                if (n < fibs.Length) return fibs[n];
+                return Fib(n - 1) + Fib(n - 2);
+            }
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < text.Length; i++)
+            {
+                var spaces = Enumerable.Repeat(' ', Fib(i));
+                sb.Append(spaces.ToArray());
+                sb.Append(text[i]);
+            }
+
+            await ReplyAsync(sb.ToString()).ConfigureAwait(false);
         }
 
         [Command("radical")]
